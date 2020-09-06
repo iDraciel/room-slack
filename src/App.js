4bac53257906d943095ca5f446d9ab7b15b80fd6
Purushottam <iDraciel@users.noreply.github.com>
2020-09-07 01:00:54 +0530

    created slack clone app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import Login from './Login';
+import {useStateValue} from './StateProvider';
 import './App.css';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
+  const[{user},dispatch]= useStateValue();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+  {!user ? (
+  <Login/>
+  ): ( <> 
+       <Header/>
+    <div className="app__body">
+      <Sidebar/>
+      <Switch>
+        <Route path="/room/:roomId">
+       <Chat/>
+        </Route>
+        <Route path="/">
+          <h1>welcome</h1>
+        </Route>
+      </Switch>
+    </div> </> )}
+   
+   </Router>
     </div>
   );
 }
